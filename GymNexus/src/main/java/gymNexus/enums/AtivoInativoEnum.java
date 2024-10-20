6ff1f767192bb1c262ae0f8aafd5415e0fb0d1ee
Marcos Leão <marcosplaignier@hotmail.com>
2024-10-20 15:23:55 -0700

    ChangeLog: Implementando deserializador de enum para poder receber o nome e encontrar a chave do enum correspondente

--- GymNexus/src/main/java/gymNexus/enums/AtivoInativoEnum.java
+++ GymNexus/src/main/java/gymNexus/enums/AtivoInativoEnum.java
@@ -1,5 +1,7 @@
 package gymNexus.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum AtivoInativoEnum {
 
     ATIVO("Ativo"),
@@ -10,4 +12,15 @@ public enum AtivoInativoEnum {
     }
 
     private String nome;
+
+//    Metodo feito para receber o nome do enum e buscar a chave correspondente
+    @JsonCreator
+    public static AtivoInativoEnum fromValue(String value) {
+        for (AtivoInativoEnum status : AtivoInativoEnum.values()) {
+            if (status.nome.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Não encontrado enum com o valor : " + value);
+    }
 }
